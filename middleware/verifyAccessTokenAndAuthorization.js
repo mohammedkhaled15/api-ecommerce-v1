8e355fdf5a867aa1c124e5709b2007e9a8dadc5e
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-03-18 17:47:04 +0200

    ..

--- middleware/verifyAccessTokenAndAuthorization.js
+++ middleware/verifyAccessTokenAndAuthorization.js
@@ -5,9 +5,7 @@ const verifyAccessTokenAndAuthorization = async (req, res, next) => {
     if (req.params.id === req.user.id) {
       next();
     } else {
-      res
-        .status(403)
-        .json("Can't access this data, it isn't Yours!");
+      res.status(403).json("Can't access this data, it isn't Yours!");
     }
   });
 };
