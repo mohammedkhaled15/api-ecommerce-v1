72a478a3ed80548c635b5748332bc41ba5cfd2da
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-04-04 02:22:34 +0200

    ..

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5000",
   "http://localhost:5173",
-  "https://admin-ecommerce-v1.netlify.app/",
+  "https://admin-ecommerce-v1.netlify.app/api/",
 ];
 
 module.exports = allowedOrigins;
