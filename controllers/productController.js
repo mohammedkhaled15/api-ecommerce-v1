f0cbbff30b859872514f2830324db8363dc56e45
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-04-09 03:32:22 +0200

    ..

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,7 +5,7 @@ const createNewProduct = async (req, res) => {
   const newProduct = new Product(req.body);
   try {
     const savedProduct = await newProduct.save();
-    res.status(200).json(savedProduct);
+    res.status(201).json(savedProduct);
   } catch (error) {
     res.status(500).json("You haven't authority to add Products");
   }
