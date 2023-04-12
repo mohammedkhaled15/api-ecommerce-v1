b9c02c28a9c00b239c76894309a09511034967c4
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-04-12 23:32:55 +0200

    ..

--- middleware/verifyAccessTokenAndAuthorization.js
+++ middleware/verifyAccessTokenAndAuthorization.js
@@ -2,7 +2,7 @@ const verifyJWT = require("./verifyJWT");
 
 const verifyAccessTokenAndAuthorization = async (req, res, next) => {
   verifyJWT(req, res, () => {
-    if (req.params.id === req.user.id) {
+    if (req.params.id === req.user.id || req.user.isAdmin === true) {
       next();
     } else {
       res.status(403).json("Can't access this data, it isn't Yours!");
