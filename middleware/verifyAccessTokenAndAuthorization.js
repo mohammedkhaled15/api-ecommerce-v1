a2bc20fc4ecd89e0132ddc97ff2533202d140ba0
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-04-13 10:20:26 +0200

    ..

--- middleware/verifyAccessTokenAndAuthorization.js
+++ middleware/verifyAccessTokenAndAuthorization.js
@@ -5,7 +5,9 @@ const verifyAccessTokenAndAuthorization = async (req, res, next) => {
     if (req.params.id === req.user.id || req.user.isAdmin === true) {
       next();
     } else {
-      res.status(403).json("Can't access this data, it isn't Yours!");
+      res
+        .status(403)
+        .json("Can't access this data, it isn't Yours nor you admin!");
     }
   });
 };
